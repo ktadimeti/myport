f19f88bbcc9ca03546bd95547fe471e6360a93de
ktadimeti <ktadimeti@live.com>
2024-09-20 18:10:21 +0530

    suppressed one var error in eslint

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Upload from "../components/component/upload";
 export default function Home() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-4xl font-bold mb-8">Portfolio Analysis</h1>
+        <h1 className="text-4xl font-bold mb-8">CAPSTAN</h1>
         <Upload />
         <footer className="flex flex-col items-center justify-center w-full h-24 border-t mt-8">
           {/* You can add any other footer content here if needed */}
