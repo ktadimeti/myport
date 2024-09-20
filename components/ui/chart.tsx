d21388725982f167eff3e2836be96c585d233cd2
ktadimeti <ktadimeti@live.com>
2024-09-20 18:43:03 +0530

    fix _unused error in chart.tsx

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -69,7 +69,7 @@ ChartContainer.displayName = "Chart"
 
 const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
   const colorConfig = Object.entries(config).filter(
-    ([_unused, config]) => config.theme || config.color
+    ([, itemConfig]) => itemConfig.theme || itemConfig.color
   )
 
   if (!colorConfig.length) {
